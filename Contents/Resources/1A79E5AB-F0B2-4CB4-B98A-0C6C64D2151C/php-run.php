5658e3a093bb98cd602f32fa1bf9989bba2f537b
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2009-03-09 20:18:48 +0000

    'php run' command is now handled by eval
    git-svn-id: http://svn.davyvandenbremt.be/svn/plugins/coda/Drupal.codaplugin@25 276e1659-e599-44dd-ad16-69ce78e6215d

---
+++ Contents/Resources/1A79E5AB-F0B2-4CB4-B98A-0C6C64D2151C/php-run.php
@@ -0,0 +1,18 @@
+#!/usr/bin/php
+
+<?php
+
+$text = stream_get_contents(STDIN);
+
+if (stripos(trim($text), '<?php') === 0) {
+  $output = eval('?>'. $text);
+}
+else {
+  $output = eval($text);
+}
+
+fwrite(STDOUT, $output); 
+
+exit(0);
+
+?>
