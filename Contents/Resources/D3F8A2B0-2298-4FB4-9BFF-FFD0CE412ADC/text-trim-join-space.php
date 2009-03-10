cca43c1a2d23a143f88bf6508515a18cc37a4026
Davy Van Den Bremt <davyvandenbremt@gmail.com>
2009-03-10 09:05:25 +0000

    - added command "text length" - added command "text join lines" - added command "text trim and join lines" - added command "text trim and join lines with space"
    git-svn-id: http://svn.davyvandenbremt.be/svn/plugins/coda/Drupal.codaplugin@28 276e1659-e599-44dd-ad16-69ce78e6215d

---
+++ Contents/Resources/D3F8A2B0-2298-4FB4-9BFF-FFD0CE412ADC/text-trim-join-space.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php
+<?php
+
+$input = "";
+
+$fp = fopen("php://stdin", "r");
+while ( $line = fgets($fp, 1024) )
+	$input .= $line;
+	
+fclose($fp);
+
+$input = rtrim($input);
+$lines = split("[\r\n]+", $input);
+for ($i = 0; $i < count($lines); $i++) {
+  $lines[$i] = trim($lines[$i]);
+}
+$input = implode(' ', $lines);
+echo $input . "\n";
+
+?>
